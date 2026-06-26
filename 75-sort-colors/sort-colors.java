2fd6ddd5aa246e7b016a6d7b85909b414ab832ad
Harshit123-SAI <harshitsaini8063@gmail.com>
2026-06-26 20:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.84%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,19 +1,26 @@
-
-     class Solution {
+class Solution {
     public void sortColors(int[] arr) {
-         int n=arr.length;
-        int noz=0,noo=0;
-        for (int i=0;i<n;i++){
-            if (arr[i]==0) noz++;
-            if (arr[i]==1) noo++;
-        }
-        for(int i=0;i<n;i++){
-            if (i<noz) arr[i]=0;
-            else if(i<noz+noo) arr[i]=1;
-            else arr[i]=2;
+        int lo = 0;
+        int hi = arr.length - 1;
+        int mid = 0;
+        while (mid <= hi) {
+            if (arr[mid] == 2) {
+                swap(arr, mid, hi);
+                hi--;
+            } 
+            else if (arr[mid] == 0) {
+                swap(arr, mid, lo);
+                lo++;
+                mid++;
+            } 
+            else {
+                mid++;
+            }
         }
     }
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
-
-        
-    
