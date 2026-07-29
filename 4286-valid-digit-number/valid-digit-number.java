ca5bc8d5a11b3720e709754a3698e632a69175d5
Harshit123-SAI <harshitsaini8063@gmail.com>
2026-07-29 21:29:59 +0530

    Time: 1 ms (98.47%) | Memory: 42.6 MB (65.65%) - LeetSync

--- 4286-valid-digit-number/valid-digit-number.java
+++ 4286-valid-digit-number/valid-digit-number.java
@@ -1,7 +1,12 @@
 class Solution {
     public boolean validDigit(int n, int x) {
-        String s = n+"";
-        if(s.contains(x+"")&& s.charAt(0)!=(x+'0')) return true;
-        return false;
+        boolean digit = false;
+        while (n > 9) {
+            if (n % 10 == x) {
+                digit = true;
+            }
+            n /= 10;
+        }
+        return digit && (n != x);
     }
 }
